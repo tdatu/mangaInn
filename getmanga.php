df6e2fed30af97fbc8b9e6a4d7ae31288dffdf31
tdatu <atdatu@gmail.com>
2014-01-10 13:50:49 -0800

    Revision made to getmanga.php

--- getmanga.php
+++ getmanga.php
@@ -11,35 +11,33 @@
 
 	//Get the URL:
 	echo "Enter the manga url: ";
-	$url = fread(STDIN, 80); //Read from cmd line
+	$url = trim(fread(STDIN, 80)); //Read from cmd line
 
 	//Get the number of episodes
 	echo "Enter the number of episodes: ";
-	$episodeNumber = fread(STDIN, 80);
+	$episodeNumber = trim(fread(STDIN, 80));
 	
 	//Get the folder name
 	echo "Enter the folder name: ";
-	$dir = fread(STDIN, 80);
-	
-	//Create a directory which ultimately 
-	//manga pages will be stored locally.
-	mkdir($dir);
-	
+	$folderName = trim(fread(STDIN, 80));
+	mkdir($folderName);
+
 	for($i = 0; $i < $episodeNumber; $i++)
 	{
 		if($i > 0)
 		{
+			//Assemble url
 			$new_url = $url;
 			$new_url .= "/page_";
 			$new_url .=  $i+1;
-			echo "old url: " . $url . "\n";
-			echo "new url: " . $new_url . "\n";
+			
+			//Get online resource
 			$html = file_get_html($new_url);
 			$img = $html->find('#imgPage');
-			//echo $img[0]->src . "\n";
 			
-	
-			$file_name = $dir. "/" . "episode" . $i . ".jpg";
+			
+			//Write on file
+			$file_name = $folderName . "/" . "episode" . $i . ".jpg";
 			$fh = fopen($file_name, 'w');
 			fwrite($fh, file_get_contents($img[0]->src));
 			fclose($fh);
@@ -47,11 +45,12 @@
 		}
 		else  // first loop
 		{
+			//Get online resource
 			$html = file_get_html($url);
 			$img = $html->find('#imgPage');
-			//echo $img[0]->src . "\n";	
 			
-			$file_name = "episode" . $i . ".jpg";
+			//Write on file	
+			$file_name = $folderName . "/" . "episode" . $i . ".jpg";
 			$fh = fopen($file_name, 'w');
 			fwrite($fh, file_get_contents($img[0]->src));
 			fclose($fh);
