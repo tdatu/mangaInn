b1a35bc8487e03de55e55ce866b5b0bf8c36c767
tdatu <atdatu@gmail.com>
2014-01-10 13:13:00 -0800

    Included in this first commit are the files: README.md getmanga.php simple_html_dom.php

---
+++ README.md
@@ -0,0 +1,16 @@
+getMangaInn is a simple script to retrieve manga pages from mangainn site for off line reading purposes.
+At the moment, this script is only been tested in mac environment although linux environment should not pose 
+any issues (in the terminal line).
+
+Files included:
+getmangainn.php
+simple_html_dom.php
+
+There are only two requirements when using the script:
+
+1) The base URL of the manga page.
+2) The number of pages to retrieve.
+
+Example:
+*Will be added soon**
+ 
---
+++ getmanga.php
@@ -0,0 +1,56 @@
+#!/usr/bin/php -q
+
+<?php
+	
+	include "simple_html_dom.php";
+
+	if(!defined ("STDIN"))
+	{
+		define ("STDIN", fopen('php://stdin', 'r'));
+	}	
+
+	//Get the URL:
+	echo "Enter the manga url: ";
+	$url = fread(STDIN, 80); //Read from cmd line
+
+	//Get the number of episodes
+	echo "Enter the number of episodes: ";
+	$episodeNumber = fread(STDIN, 80);
+	
+	//Get the folder name
+	echo "Enter the folder name: ";
+	$dir = fread(STDIN, 80);
+	
+	for($i = 0; $i < $episodeNumber; $i++)
+	{
+		if($i > 0)
+		{
+			$new_url = $url;
+			$new_url .= "/page_";
+			$new_url .=  $i+1;
+			echo "old url: " . $url . "\n";
+			echo "new url: " . $new_url . "\n";
+			$html = file_get_html($new_url);
+			$img = $html->find('#imgPage');
+			//echo $img[0]->src . "\n";
+			
+	
+			$file_name = $dir. "/" . "episode" . $i . ".jpg";
+			$fh = fopen($file_name, 'w');
+			fwrite($fh, file_get_contents($img[0]->src));
+			fclose($fh);
+			
+		}
+		else  // first loop
+		{
+			$html = file_get_html($url);
+			$img = $html->find('#imgPage');
+			//echo $img[0]->src . "\n";	
+			
+			$file_name = "episode" . $i . ".jpg";
+			$fh = fopen($file_name, 'w');
+			fwrite($fh, file_get_contents($img[0]->src));
+			fclose($fh);
+		}
+	}
+?>
